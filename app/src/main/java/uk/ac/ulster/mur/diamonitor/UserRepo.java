13c34d03dbceb743124ef6bcff67dbf0df4f8d00
Conor-m <murf08@gmail.com>
2018-03-13 20:18:21 +0000

    DB changes - added User Details Table

--- app/src/main/java/uk/ac/ulster/mur/diamonitor/UserRepo.java
+++ app/src/main/java/uk/ac/ulster/mur/diamonitor/UserRepo.java
@@ -1,7 +1,6 @@
 package uk.ac.ulster.mur.diamonitor;
 
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 
@@ -31,42 +30,14 @@ public class UserRepo {
         values.put(User.KEY_MinRange, user.getMinRange());
 
         // Inserting Row
-        userId=(int)db.insert(Blood.TABLE, null, values);
+        userId=(int)db.insert(User.TABLE, null, values);
         DatabaseManager.getInstance().closeDatabase();
         return userId;
     }
 
     public void delete( ) {
         SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
-        db.delete(Blood.TABLE, null,null);
+        db.delete(User.TABLE, null,null);
         DatabaseManager.getInstance().closeDatabase();
-    }
-
-    public String databaseToString(){
-        String dbString = "";
-        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
-        String query = "SELECT * FROM " + Blood.TABLE + " WHERE 1";// SELECT ALL
-
-        //Cursor points to a location in your results
-        Cursor recordSet = db.rawQuery(query, null);
-        //Move to the first row in your results
-        recordSet.moveToFirst();
-
-        //Position after the last row means the end of the results
-        while (!recordSet.isAfterLast()) {
-            // null could happen if we used our empty constructor
-            if (recordSet.getString(recordSet.getColumnIndex("Reading")) != null) {
-                dbString += "||";
-                dbString += recordSet.getString(recordSet.getColumnIndex("BloodId"));
-                dbString += "||";
-                dbString += recordSet.getString(recordSet.getColumnIndex("Reading"));
-                dbString += "||";
-                recordSet.getString(recordSet.getColumnIndex("Time"));
-                dbString += "||\n";
-            }
-            recordSet.moveToNext();
-        }
-        db.close();
-        return dbString;
     }
 }
