ac7525fda9e13330ef9d4e1db28ed213fae2b8a8
Conor-m <murf08@gmail.com>
2018-07-13 17:55:55 +0100

    Changes to images

--- app/build/generated/source/r/debug/uk/ac/ulster/mur/diamonitor/R.java
+++ app/build/generated/source/r/debug/uk/ac/ulster/mur/diamonitor/R.java
@@ -3670,10 +3670,11 @@ public final class R {
     public static final int notification_tile_bg=0x7f070076;
     public static final int notify_panel_notification_icon_bg=0x7f070077;
     public static final int plate=0x7f070078;
-    public static final int splash_screen=0x7f070079;
-    public static final int tooltip_frame_dark=0x7f07007a;
-    public static final int tooltip_frame_light=0x7f07007b;
-    public static final int warningicon=0x7f07007c;
+    public static final int powered_by_fatsecret=0x7f070079;
+    public static final int splash_screen=0x7f07007a;
+    public static final int tooltip_frame_dark=0x7f07007b;
+    public static final int tooltip_frame_light=0x7f07007c;
+    public static final int warningicon=0x7f07007d;
   }
   public static final class id {
     public static final int ALT=0x7f080000;
